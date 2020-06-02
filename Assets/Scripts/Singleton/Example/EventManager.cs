13584b9857e0843432d30376b73b056918e9dfff
agodinez <agodinez@teachlabs.com>
2020-06-02 13:22:06 +0200

    Singleton Example

---
+++ Assets/Scripts/Singleton/Example/EventManager.cs
@@ -0,0 +1,41 @@
+﻿
+using System.Collections.Generic;
+using UnityEngine.Events;
+
+namespace SingletonExample
+{
+  public class EventManager : Singleton<EventManager>
+  {
+    private Dictionary<string, UnityEvent> events = new Dictionary<string, UnityEvent>();
+
+    public static void StartListening(string eventName, UnityAction listener)
+    {
+      UnityEvent thisEvent = null;
+
+      if (Instance.events.TryGetValue(eventName, out thisEvent))
+        thisEvent.AddListener(listener);
+      else
+      {
+        thisEvent = new UnityEvent();
+        thisEvent.AddListener(listener);
+        Instance.events.Add(eventName, thisEvent);
+      }
+    }
+
+    public static void StopListening(string eventName, UnityAction listener)
+    {
+      UnityEvent thisEvent = null;
+
+      if (Instance.events.TryGetValue(eventName, out thisEvent))
+        thisEvent.RemoveListener(listener);
+    }
+
+    public static void TriggerEvent(string eventName)
+    {
+      UnityEvent thisEvent = null;
+
+      if (Instance.events.TryGetValue(eventName, out thisEvent))
+        thisEvent.Invoke();
+    }
+  }
+}
---
+++ Assets/Scripts/Singleton/Example/PeopleReaction.cs
@@ -0,0 +1,29 @@
+﻿
+using UnityEngine;
+
+namespace SingletonExample
+{
+  public class PeopleReaction : MonoBehaviour
+  {
+    void OnEnable()
+    {
+      EventManager.StartListening("Shoot", Shoot);
+    }
+
+    void OnDisable()
+    {
+      EventManager.StopListening("Shoot", Shoot);
+    }
+
+    void Shoot()
+    {
+      Debug.Log("People start screaming !");
+    }
+
+    void Update()
+    {
+      if (Input.GetKeyDown("s"))
+        EventManager.TriggerEvent("Shoot");
+    }
+  }
+}
